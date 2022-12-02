0a97b8b0a367c080779a12b431b601301fa1d00e
pocketxjl <xuejl2001@mail.ustc.edu.cn>
2022-12-02 11:10:22 +0800

    get wechat account

---
+++ Contact_Information_Extraction/get_wechat_contact.py
@@ -0,0 +1,187 @@
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
+import numpy as np
+import pandas as pd
+from scipy.special import softmax
+import csv
+from simpletransformers.ner import NERModel, NERArgs
+import jieba
+import logging
+from sklearn.metrics import f1_score,  accuracy_score
+from sklearn.model_selection import train_test_split
+import torch
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
+
+NER_model_path = '/data/jlxue/RBSEO_wechat_NER_train_model'
+classify_model_path = '/data/jlxue/RBSEO_contact_classifier_train_model'
+
+
+def get_wechat_contact(terms_all_list, wechat_contact):
+    unicode_similar_nums = [
+        '0ΟοσОо０Օօסه٥ھہە۵߀०০੦૦ଠ୦௦ం౦ಂ೦ംഠ൦ං๐໐ဝ၀ჿዐᴏᴑℴⲞⲟⵔ〇ꓳꬽﮦﮧﮨﮩﮪﮫﮬﮭﻩﻪﻫﻬ０Ｏｏ𐊒𐊫𐐄𐐬𐓂𐓪𐔖𑓐𑢵𑣈𑣗𑣠𝐎𝐨𝑂𝑜𝑶𝒐𝒪𝓞𝓸𝔒𝔬𝕆𝕠𝕺𝖔𝖮𝗈𝗢𝗼𝘖𝘰𝙊𝙤𝙾𝚘𝚶𝛐𝛔𝛰𝜊𝜎𝜪𝝄𝝈𝝤𝝾𝞂𝞞𝞸𝞼𝟎𝟘𝟢𝟬𝟶𞸤𞹤𞺄🯰',
+        '1|ıƖ１ǀɩɪ˛２ͺΙιІіӀӏ׀וןا١۱ߊᎥᛁιℐℑℓℹⅈⅠⅰⅼ∣⍳⏽Ⲓⵏꓲꙇꭵﺍﺎ１Ｉｉｌ￨𐊊𐌉𐌠𑣃𖼨𝐈𝐢𝐥𝐼𝑖𝑙𝑰𝒊𝒍𝒾𝓁𝓘𝓲𝓵𝔦𝔩𝕀𝕚𝕝𝕴𝖎𝖑𝖨𝗂𝗅𝗜𝗶𝗹𝘐𝘪𝘭𝙄𝙞𝙡𝙸𝚒𝚕𝚤𝚰𝛊𝛪𝜄𝜤𝜾𝝞𝝸𝞘𝞲𝟏𝟙𝟣𝟭𝟷𞣇𞸀𞺀🯱',
+        '2ƧϨᒿꙄꛯꝚ２𝟐𝟚𝟤𝟮𝟸🯲',
+        '3ƷȜЗӠⳌꝪꞫ３𑣊𖼻𝈆𝟑𝟛𝟥𝟯𝟹🯳',
+        '4Ꮞ４𑢯𝟒𝟜𝟦𝟰𝟺🯴',
+        '5Ƽ５𑢻𝟓𝟝𝟧𝟱𝟻🯵',
+        '6бᏮ６Ⳓ６𑣕𝟔𝟞𝟨𝟲𝟼🯶',
+        '7７𐓒𑣆𝈒𝟕𝟟𝟩𝟳𝟽🯷',
+        '8Ȣȣ８৪੪ଃ８𐌚𝟖𝟠𝟪𝟴𝟾𞣋🯸',
+        '9৭੧୨൭ⳊꝮ９𑢬𑣌𑣖𝟗𝟡𝟫𝟵𝟿🯹'
+    ]
+    cuda_available = torch.cuda.is_available()
+
+    args = ClassificationArgs(eval_batch_size = 32, use_multiprocessing_for_evaluation=False)
+    model = ClassificationModel('roberta', classify_model_path, num_labels=len(labels_list), use_cuda=cuda_available, args = args)
+
+    wechat_terms = []
+    predictions, raw_outputs = model.predict(terms_all_list)
+
+    for index, term in enumerate(terms_all_list):
+        if labels_list[predictions[index]] == 'wechat':
+            wechat_terms.append(term)
+
+    print(f'Finish contact classification, get wechat term {len(wechat_terms)}')
+    #-------------------------------------------------------------------------------------------------
+    sentences = []
+    sentences_raw = []
+    for term in wechat_terms:
+        term_raw = term
+        for char in term:
+            for simple_num, similar_num in enumerate(unicode_similar_nums):
+                if char in similar_num:
+                    term = term.replace(char, str(simple_num))
+                    break
+        seg = jieba.cut(term)
+        sentence = ''
+        for s in seg:
+            sentence += s + ' '
+        # Token indices sequence length is longer than the specified maximum sequence length for this model (683 > 512). 
+        # Running this sequence through the model will result in indexing errors
+        if len(sentence) < 512:
+            sentences_raw.append(term_raw)
+            sentences.append(sentence)
+
+
+    # Create a NERModel
+    labels = ['B-contact', 'I-contact', 'O']
+
+    cuda_available = torch.cuda.is_available()
+    args = NERArgs(use_multiprocessing_for_evaluation=False)
+    model = NERModel(
+        "roberta",
+        NER_model_path,
+        labels=labels,
+        use_cuda=cuda_available,
+        args=args
+    )
+
+    # # Predictions on arbitary text strings
+    predictions, raw_outputs = model.predict(sentences)
+    
+    for n, (preds, outs) in enumerate(zip(predictions, raw_outputs)):
+        result = ['', '', '', '', '']
+        result[0] = sentences_raw[n]
+        flag = 0
+        result_index = 1
+        try:
+            for pred, out in zip(preds, outs):
+                key = list(pred.keys())[0]
+                if flag == 0:
+                    if pred[key] == 'B-contact':
+                        flag = 1
+                        result[result_index] += key
+                elif flag == 1:
+                    if pred[key] == 'I-contact':
+                        flag = 2
+                        result[result_index] += key
+                    elif pred[key] == 'O':
+                        result_index += 1
+                        flag = 0
+                    elif pred[key] == 'B-contact':
+                        result_index += 1
+                        flag = 1
+                        result[result_index] += key
+                elif flag == 2:
+                    if pred[key] == 'I-contact':
+                        flag = 2
+                        result[result_index] += key
+                    elif pred[key] == 'O':
+                        result_index += 1
+                        flag = 0
+                    elif pred[key] == 'B-contact':
+                        result_index += 1
+                        flag = 1
+                        result[result_index] += key
+        except:
+            print("\n___________________________")
+            print("Sentence: ", sentences_raw[n])
+            for pred, out in zip(preds, outs):
+                key = list(pred.keys())[0]
+                new_out = out[key]
+                preds = list(softmax(np.mean(new_out, axis=0)))
+                print(key, pred[key], preds[np.argmax(preds)], preds)
+        if result[1] != '' and len(result[1]) > 4:
+            account = result[1]
+            wechat_contact[account] = sentences_raw[n]
+
+
+data = []
+
+with open('data/Google/positive_data_predicted_fromcontact.txt', 'r', encoding = 'utf-8') as fp:
+    data += fp.readlines()
+
+with open('data/Google/positive_data_predicted_fromsite.txt', 'r', encoding = 'utf-8') as fp:
+    data += fp.readlines()
+
+with open('data/Bing/positive_data_predicted.txt', 'r', encoding = 'utf-8') as fp:
+    data += fp.readlines()
+
+with open('data/Baidu/positive_data_predicted.txt', 'r', encoding = 'utf-8') as fp:
+    data += fp.readlines()
+
+with open('data/Sogou/positive_data_predicted.txt', 'r', encoding = 'utf-8') as fp:
+    data += fp.readlines()
+
+terms_all = set()
+for item in data:
+    term, link, kwd, timestamp, pagenum = eval(item)
+    terms_all.add(term)
+
+print(f'Finish Getting terms, get {len(terms_all)} terms')
+
+#-------------------------------------------------------------------------------------------------
+labels_list = ['website', 'wechat', 'qq', 'telegram', 'others']
+labels_dict = {'website':0, 'wechat':1, 'qq':2, 'telegram':3, 'others':4}
+terms_all_list = []
+wechat_contact = {}
+num = 0
+index_term = 0
+
+for item in terms_all:
+    num += 1
+    index_term += 1
+    terms_all_list.append(item)
+    if num >= 50000:
+        print(f'Get {len(terms_all_list)} positive terms')
+        get_wechat_contact(terms_all_list, wechat_contact)
+        logging.info(f'Finished terms {index_term} of {len(terms_all)} for extracting wechat contact, get {len(wechat_contact)} wechat contacts')
+        num = 0
+        terms_all_list = []
+
+
+print(f'Get {len(terms_all_list)} positive terms')
+
+get_wechat_contact(terms_all_list, wechat_contact)
+
+print(f'Finish extract wechat contact, get {len(wechat_contact)}')
+with open('data/wechat_contact.txt', 'w', encoding='utf-8') as fp:
+    for item in wechat_contact:
+        fp.write(str((item, wechat_contact[item])))
+        fp.write('\n')
+
