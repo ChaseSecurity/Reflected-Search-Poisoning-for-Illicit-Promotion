925136ac9f9825bc71341f17182793206b7a348a
31 <sywu31@qq.com>
2023-03-05 11:03:58 +0800

    update

--- Website_Timemachine/App/app_detection.py
+++ Website_Timemachine/App/app_detection.py
@@ -7,8 +7,9 @@ import time
 data_base_dir = "/data2/sangyiwu/RBSEO_Cybercrime_TimeMachine/output_website"
 
 def is_download_related(content):
-    keywords = ['下载', '安卓app', '苹果app', 'downbtn', 'downsoft', 'download']
+    keywords = ['下载', '安卓app', '苹果app', 'download', 'androidapp', 'iosapp', 'androiddown', 'downapp', 'appbtn', 'downbtn', 'downsoft']
     content.lower()
+    content.replace('_', '').replace('-', '').replace(' ', '')
     for keyword in keywords:
         if keyword in content:
             return (True, keyword)
@@ -36,9 +37,13 @@ if __name__ == "__main__":
 
         result = []
         total = 0
+        domains = set()
+        domains_app = set()
         for res_stat in res_stats:
             if res_stat['is_success'] == True:
                 total += 1
+                domains.add(res_stat['domain'])
+
                 res_dir = f"{data_base_dir}/{res_stat['provider']}/{res_stat['result_basedir']}"
                 har_file_path = f"{res_dir}/test.har"
                 (app_likely, keyword) = (False, '')
@@ -56,15 +61,17 @@ if __name__ == "__main__":
                                         (app_likely, keyword) = is_download_related(content['text'])
                                         if app_likely == True:
                                             result.append([res_stat['domain'], res_stat['result_basedir']])
+                                            domains_app.add(res_stat['domain'])
                                             break
-        logging.info(f"Among {total} urls, {len(result)} may contain APP download content.")
-
+        logging.info(f"Among {total} urls, {len(result)}({len(result)/total}) may contain APP download content.")
+        logging.info(f"Among {len(domains)} domains, {len(domains_app)}({len(domains_app)/len(domains)}) may contain APP download content.")
+        
         with open(f"app_{options.date}.csv", "w", encoding="utf-8", newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['domain', 'result_dir'])
             csvwriter.writerows(result)
         logging.info(f"Results have been written to the file app_{options.date}.csv.")
-        logging.info(f"Done with the cost of {time.time()-start_time} seconds.")
+        logging.info(f"Done {options.date} with the cost of {time.time()-start_time} seconds.")
     
     except Exception as e:
         logging.warning(e)
