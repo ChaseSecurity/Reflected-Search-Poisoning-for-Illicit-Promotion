63aca6192c72ac93ba0fb5b413bd76b8c63c3ccd
31 <sywu31@qq.com>
2023-04-03 14:22:53 +0800

    update

---
+++ Telegram_Timemachine/Classifier/train_classify_telegram_messages.py
@@ -0,0 +1,104 @@
+# train_classify_seo_terms.py
+
+"""
+Follow tutorials listed below
+  https://github.com/ThilinaRajapakse/simpletransformers
+"""
+version = 'v5'
+output_path = f'/data/sangyiwu/RBSEO_classifier_train_model/{version}'
+
+from simpletransformers.classification import MultiLabelClassificationModel
+import pandas as pd
+import logging
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import torch
+import csv
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
+
+
+#--------------------------Read Classes---------------------------
+labels_list = []
+labels_dict = {}
+with open(f'./{version}/labels.txt', 'r', encoding='utf-8') as fp:
+    reader = fp.readlines()
+for index, item in enumerate(reader):
+    label = item[:-1]
+    labels_dict[label] = index
+    labels_list.append(label)
+
+logging.info(f"Finish getting labels, get {len(labels_list)} classes. ")
+
+#---------------------------------Get Train Set---------------------------
+with open(f'./{version}/labeled_messages.csv', mode='r', encoding='utf-8') as fp:
+    reader = csv.reader(fp)
+    next(reader)
+    samples = []
+    for row in reader:
+        one_data = []
+        one_data.append(row[2])
+        one_data_label = [0 for i in range(len(labels_list))]
+        if row[4] != '':
+            one_data_label[labels_dict[row[4]]] = 1
+        if row[5] != '':
+            one_data_label[labels_dict[row[5]]] = 1
+        if row[6] != '':
+            one_data_label[labels_dict[row[6]]] = 1
+        if row[7] != '':
+            one_data_label[labels_dict[row[7]]] = 1
+        samples.append([row[2], one_data_label])
+
+logging.info(f"Loaded {len(samples)} samples.")
+
+sample_df = pd.DataFrame(samples)
+sample_df.columns = ["text", "labels"]
+train_df, eval_df = train_test_split(sample_df, test_size=0.2)
+
+# Optional model configuration
+cuda_available = torch.cuda.is_available()
+
+# Create a ClassificationModel
+model = MultiLabelClassificationModel(
+    "bert",
+    "bert-base-multilingual-cased",
+    use_cuda=cuda_available,
+    args={
+        "reprocess_input_data": True,
+        "overwrite_output_dir": True,
+        "num_train_epochs": 50,
+        "output_dir": output_path
+    },
+    num_labels=len(labels_list),
+)
+
+def precision_score_multilabel(y_true, y_pred):
+    y_pred = y_pred.round()
+    return precision_score(y_true, y_pred, average='weighted')
+
+def recall_score_multilabel(y_true, y_pred):
+    y_pred = y_pred.round()
+    return recall_score(y_true, y_pred, average='weighted')
+
+def f1_score_multilabel(y_true, y_pred):
+    y_pred = y_pred.round()
+    return f1_score(y_true, y_pred, average='weighted')
+
+# Train the model
+model.train_model(train_df, eval_df, precision = precision_score_multilabel, recall = recall_score_multilabel, f1 = f1_score_multilabel)
+
+# Evaluate the model
+result, model_outputs, wrong_predictions = model.eval_model(eval_df, precision = precision_score_multilabel, recall = recall_score_multilabel, f1 = f1_score_multilabel)
+
+
+wrong_predictions_to_save = []
+for wp in wrong_predictions:
+    idx = wp.guid
+    prediction = model_outputs[idx]
+    wrong_predictions_to_save.append([wp.text_a, prediction, wp.label])
+
+with open(f'./{version}/wrong_predictions.csv', 'w', encoding='utf-8') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['text', 'prediction', 'label'])
+    csvwriter.writerows(wrong_predictions_to_save)
